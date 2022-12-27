27a7ef8c243a2d938578141a29162c4e100c9e33
ccall <c.calli@diten.tech>
2022-12-27 22:06:52 +0300

    Splash

---
+++ app/src/main/java/com/zerdasoftware/jsoupwebscraping/SplashActivity.kt
@@ -0,0 +1,10 @@
+package com.zerdasoftware.jsoupwebscraping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
